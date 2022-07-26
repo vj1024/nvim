bd8a7027db5589671c8838409f9b8abe6455667c
VJ <vj1024@sina.com>
2022-07-26 16:44:39 +0800

    update options

--- init.vim
+++ init.vim
@@ -17,3 +17,5 @@ omap s :normal vs<CR>
 "}
 
 " vim.g.vim_markdown_folding_disabled = 1
+
+set statusline=
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -5,20 +5,20 @@ vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
 vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
 vim.opt.hlsearch = true                         -- highlight all matches on previous search pattern
-vim.opt.ignorecase = true                       -- ignore case in search patterns
-vim.opt.mouse = ""                             -- allow the mouse to be used in neovim
+vim.opt.ignorecase = false                      -- ignore case in search patterns
+vim.opt.mouse = ""                              -- allow the mouse to be used in neovim
 vim.opt.pumheight = 10                          -- pop up menu height
 vim.opt.showmode = false                        -- we don't need to see things like -- INSERT -- anymore
 vim.opt.showtabline = 0                         -- always show tabs
 vim.opt.smartcase = true                        -- smart case
-vim.opt.smartindent = true                      -- make indenting smarter again
+vim.opt.smartindent = false                     -- make indenting smarter again
 vim.opt.splitbelow = true                       -- force all horizontal splits to go below current window
 vim.opt.splitright = true                       -- force all vertical splits to go to the right of current window
 vim.opt.swapfile = false                        -- creates a swapfile
 vim.opt.termguicolors = true                    -- set term gui colors (most terminals support this)
 vim.opt.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
-vim.opt.undofile = false                         -- enable persistent undo
-vim.opt.updatetime = 300                        -- faster completion (4000ms default)
+vim.opt.undofile = false                        -- enable persistent undo
+vim.opt.updatetime = 1000                       -- faster completion (4000ms default)
 vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 vim.opt.expandtab = true                        -- convert tabs to spaces
 vim.opt.shiftwidth = 4                          -- the number of spaces inserted for each indentation
@@ -30,12 +30,12 @@ vim.opt.showcmd = false
 vim.opt.ruler = false
 vim.opt.numberwidth = 4                         -- set number column width to 2 {default 4}
 vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
-vim.opt.wrap = true                            -- display lines as one long line
-vim.opt.scrolloff = 8                           -- is one of my fav
+vim.opt.wrap = true                             -- display lines as one long line
+vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
 vim.opt.fillchars.eob=" "
 vim.opt.shortmess:append("c")
-vim.opt.whichwrap:append("<,>,[,]")           -- Allow specified keys that move the cursor left/right to move to the previous/next line when the cursor is on the first/last character in the line.
+vim.opt.whichwrap:append("<,>,[,]")             -- Allow specified keys that move the cursor left/right to move to the previous/next line when the cursor is on the first/last character in the line.
 vim.opt.iskeyword:append("-")
 vim.opt.relativenumber = false
