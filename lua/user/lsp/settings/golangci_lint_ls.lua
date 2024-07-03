36e085f6db17e21b57ec9f19da87400870e9bef5
VJ <vj1024@sina.com>
2024-07-03 19:16:20 +0800

    clean repo

--- lua/user/lsp/settings/golangci_lint_ls.lua
+++
@@ -1,7 +0,0 @@
-local util = require "lspconfig/util"
-return {
-  filetypes = {"go", "gomod"},
-  root_dir =  function(fname)
-    return util.root_pattern('go.work')(fname) or util.root_pattern('go.mod', '.golangci.yaml', '.git')(fname) or fname
-  end
-}
